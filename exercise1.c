eab8e3d03a5246fffeaf9cdb0acc5ea8bf3b6280
audreyemmely <aerv@ic.ufal.br>
2019-09-22 15:29:13 -0300

    First exercise

---
+++ exercise1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{   
+    int numbers;
+        
+    while (scanf("%d", &numbers) && numbers != 0)
+    { 
+        printf("%d\n", numbers*numbers);
+    }
+    return 0;
+}
+testando
