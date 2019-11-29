2254790bebe57fc516dd26319478c0bd23732361
audreyemmely <aerv@ic.ufal.br>
2019-11-29 17:24:31 -0300

    ponteiros como referência em funções

--- variavelPorReferencia.c
+++ variavelPorReferencia.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-void soma(int piValorA, int piValor B, int *piResultado){
-  printf("Endereco de piResultado = %d\n", piResultado); 
+void soma(int piValorA, int piValorB, int *piResultado){
+  printf("Endereco de piResultado = %p\n", piResultado); 
   /* o valor está sendo colocado diretamente na memória */
   *piResultado = piValorA + piValorB;
 }
@@ -9,12 +9,15 @@ void soma(int piValorA, int piValor B, int *piResultado){
 int main(void) {
 int iValorA, iValorB, iResultado;
 
-printf("Entre com os valores: ")
+printf("Entre com os valores: ");
 
 scanf("%d %d", &iValorA, &iValorB);
 
-printf("Endereco de iResultado = %d\n", &iResultado);
+printf("Endereco de iResultado = %p\n", &iResultado);
 
+soma(iValorA, iValorB, &iResultado);
+
+printf("Soma: %d\n", iResultado);
 
 return 0;
 }
