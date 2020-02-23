4c8cd3d6382c72428467fa8c1d8c3d5698a1b7d3
Audrey <aerv@ic.ufal.br>
2020-02-23 15:54:44 +0000

    exemplo de uso ponteiro para ponteiro

---
+++ ponteiroParaPonteiro.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int funcao(int**piParametro)
+{
+  printf("%d\n", &piParametro);
+  printf("%d\n", piParametro);
+  printf("%d\n", *piParametro);
+  printf("%d\n", **piParametro);
+  return 0;
+}
+
+int main(void) 
+{
+  int *piVariavel;
+  *piVariavel = (int*)malloc(sizeof(int));
+  *piVariavel = 20;
+  printf("%d\n", &piVariavel);
+  printf("%d\n", piVariavel);
+  printf("%d\n", *piVariavel);
+
+  funcao(&piVariavel);
+
+  return 0;
+}
